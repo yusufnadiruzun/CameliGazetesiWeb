e6dffe4d3236ad04eebea6faf3562a36c6a10080
yusufnadiruzun <yusufnadir.uzun@stu.fsm.edu.tr>
2022-04-29 02:42:11 +0300

    createList

--- src/Components/Admin/NewsList.js
+++ src/Components/Admin/NewsList.js
@@ -1,34 +1,48 @@
+import {useSelector} from 'react-redux'
+
+import {Button, Input} from 'reactstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
 function NewsList() {
+
+
+  const {news} = useSelector((state) => state.result )
+  const removeReport = () =>{
+    
+  }
+  
   return (
     <div>
-      <table class="table">
+      <table className="table">
         <thead>
+
           <tr>
             <th scope="col">Id</th>
+            <th scope="col">Tarih</th>
             <th scope="col">Başlık</th>
-            <th scope="col">İçerik</th>
             <th scope="col">Kategori</th>
-            <th scope="col">Kategori</th>
+            <th scope="col">Şehir</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+          {news.map(report =>
+            <tr>
+            <th scope="row">{report.id}</th>
+            <td>{report.date}</td>
+            <td>{report.Desciription}</td>
+            <td>{report.category}</td>
+            <td>{report.location}</td>
+            <td>
+                <Button
+                  color="danger"
+                  onClick={() => removeReport() }
+                >
+                  Sil
+                </Button>
+              </td>
           </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+            )}
+          
+        
         </tbody>
       </table>
     </div>
